813dffc57f5b64b89562b7504256535977c52756
root <root@frabbi-001.novalocal>
2014-11-16 13:59:56 +0000

    movielens schema and data loader sql

--- sql/movielens_dataload.sql
+++ sql/movielens_dataload.sql
@@ -12,4 +12,59 @@ ENCLOSED BY ''
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
+LOAD DATA INFILE '/home/ubuntu/.projects/futuregrid-452/data/movielens/hetrec2011/movie_directors.dat' 
+INTO TABLE movie_directors
+FIELDS TERMINATED BY '\t' 
+ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/home/ubuntu/.projects/futuregrid-452/data/movielens/hetrec2011/movie_countries.dat' 
+INTO TABLE movie_countries
+FIELDS TERMINATED BY '\t' 
+ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/home/ubuntu/.projects/futuregrid-452/data/movielens/hetrec2011/movie_locations.dat' 
+INTO TABLE movie_locations 
+FIELDS TERMINATED BY '\t' 
+ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/home/ubuntu/.projects/futuregrid-452/data/movielens/hetrec2011/movie_tags.dat' 
+INTO TABLE movie_tags 
+FIELDS TERMINATED BY '\t' 
+ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/home/ubuntu/.projects/futuregrid-452/data/movielens/hetrec2011/movie_genres.dat' 
+INTO TABLE movie_genres
+FIELDS TERMINATED BY '\t' 
+ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/home/ubuntu/.projects/futuregrid-452/data/movielens/hetrec2011/tags.dat' 
+INTO TABLE tags 
+FIELDS TERMINATED BY '\t' 
+ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/home/ubuntu/.projects/futuregrid-452/data/movielens/hetrec2011/user_ratedmovies.dat' 
+INTO TABLE user_ratedmovies
+FIELDS TERMINATED BY '\t' 
+ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/home/ubuntu/.projects/futuregrid-452/data/movielens/hetrec2011/user_taggedmovies.dat' 
+INTO TABLE user_taggedmovies
+FIELDS TERMINATED BY '\t' 
+ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
--- sql/movielens_schema.sql
+++ sql/movielens_schema.sql
@@ -29,9 +29,103 @@ DROP TABLE IF EXISTS movie_actors;
 
 CREATE TABLE movie_actors(
   movieID INT NOT NULL,
-  actorID INT NOT NULL,
+  actorID VARCHAR(255) NOT NULL,
   actorName VARCHAR(255) NULL,
   ranking INT NULL,
   PRIMARY KEY (movieID,actorID)
 );
+
+
+DROP TABLE IF EXISTS movie_countries;
+
+CREATE TABLE movie_countries(
+  movieID INT NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  PRIMARY KEY (movieID,country)
+);
+
+
+DROP TABLE IF EXISTS movie_directors;
+
+CREATE TABLE movie_directors(
+  movieID INT NOT NULL,
+  directorID VARCHAR(255) NOT NULL,
+  directorName VARCHAR(255) NULL,
+  PRIMARY KEY (movieID,directorID)
+);
+
+
+DROP TABLE IF EXISTS movie_genres;
+
+CREATE TABLE movie_genres(
+  movieID INT NOT NULL,
+  genre VARCHAR(255) NOT NULL,
+  PRIMARY KEY (movieID,genre)
+);
+
+
+DROP TABLE IF EXISTS movie_locations;
+
+CREATE TABLE movie_locations(
+  movieID INT NOT NULL,
+  location1 VARCHAR(255) NULL,
+  location2 VARCHAR(255) NULL,
+  location3 VARCHAR(255) NULL,
+  location4 VARCHAR(255) NULL,
+  PRIMARY KEY (movieID,location1,location2,location3,location4)
+);
+
+
+DROP TABLE IF EXISTS movie_tags;
+
+CREATE TABLE movie_tags(
+  movieID INT NOT NULL,
+  tagID INT NOT NULL,
+  tagWeight INT NULL,
+  PRIMARY KEY (movieID,tagID)
+);
+
+
+DROP TABLE IF EXISTS tags;
+
+CREATE TABLE tags(
+  id INT NOT NULL,
+  value VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+DROP TABLE IF EXISTS user_ratedmovies;
+
+CREATE TABLE user_ratedmovies(
+  userID INT NOT NULL,
+  movieID INT NOT NULL,
+  rating FLOAT(2,1) NOT NULL,
+  date_day INT(2) NULL,
+  date_month INT(2) NULL,
+  date_year INT(4) NULL,
+  date_hour INT(2) NULL,
+  date_minute INT(2) NULL,
+  date_second INT(2) NULL,
+  PRIMARY KEY (userID,movieID)
+);
+
+
+DROP TABLE IF EXISTS user_taggedmovies;
+
+CREATE TABLE user_taggedmovies(
+  userID INT NOT NULL,
+  movieID INT NOT NULL,
+  tagID INT NOT NULL,
+  date_day INT(2) NULL,
+  date_month INT(2) NULL,
+  date_year INT(4) NULL,
+  date_hour INT(2) NULL,
+  date_minute INT(2) NULL,
+  date_second INT(2) NULL,
+  PRIMARY KEY (userID,movieID,tagID)
+);
+
+
+
 
