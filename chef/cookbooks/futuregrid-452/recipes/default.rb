a4a9a0e3c61b88005572b9d531d4c1b662f685f2
root <root@frabbi-001.novalocal>
2014-11-16 14:04:52 +0000

    futuregrid-452 cookbook updated

--- chef/cookbooks/futuregrid-452/recipes/default.rb
+++ chef/cookbooks/futuregrid-452/recipes/default.rb
@@ -11,19 +11,45 @@ include_recipe "mysql::client"
 include_recipe "mysql::server"
 include_recipe "database::mysql"
 
+# Mysql connection info
+mysql_connection_info = {
+  :host     => 'localhost',
+  :username => 'root',
+  :password => node['mysql']['server_root_password']
+}
 
 # Create a mysql database
 mysql_database node['futuregrid-452']['database'] do
-  connection ({:host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password']})
+  connection mysql_connection_info
   action :create
 end
 
 # Create mysql user
 mysql_database_user node['futuregrid-452']['db_username'] do
-  connection ({:host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password']})
+  connection mysql_connection_info
   password node['futuregrid-452']['db_password']
   database_name node['futuregrid-452']['database']
   privileges [:select,:update,:insert,:create,:delete]
   action :grant
 end
 
+# Query a database
+mysql_database node['futuregrid-452']['database'] do
+  connection mysql_connection_info
+  sql        'flush privileges'
+  action     :query
+end
+
+# Create movielens database schema
+mysql_database node['futuregrid-452']['database'] do
+  connection mysql_connection_info
+  sql { ::File.open('/home/ubuntu/.projects/futuregrid-452/sql/movielens_schema.sql').read }
+  action :query
+end
+
+# Load movielens data into mysql database
+mysql_database node['futuregrid-452']['database'] do
+  connection mysql_connection_info
+  sql { ::File.open('/home/ubuntu/.projects/futuregrid-452/sql/movielens_dataload.sql').read }
+  action :query
+end
--- chef/roles/java.rb
+++ chef/roles/java.rb
@@ -3,7 +3,6 @@ description "Install Oracle Java"
 default_attributes(
   "java" => {
     "install_flavor" => "oracle",
-    "jdk_version" => "6",
     "set_etc_environment" => true,
     "oracle" => {
       "accept_oracle_download_terms" => true
